b3682bf24955bfb371a0e8994903a7e70d7b726e
sironimo <siro.fritzmann@bluewin.ch>
2018-01-28 16:00:09 +0100

    100er apps in simulation, add plots

--- plots.R
+++ plots.R
@@ -2,6 +2,8 @@
 library(ggthemes)
 library(ggplot2)
 library(dplyr)
+#
+
 #library(reshape2)
 
 simulations_out <- read.csv("data/simulations.csv")
@@ -46,7 +48,7 @@ ggsave(filename = "graphs/norm_energy.pdf", device = "pdf", scale = 0.8)
 
 
 #Calculate avg, sd, etc.----
-aggregated <- simulations_out %>%
+aggregated <- simulations_out %>% filter(Environment == "combined_cluster") %>%
   group_by(Scheduler) %>%
   summarise(avg_time = mean(time),
             first_quant_time = quantile(time,0.25),
@@ -71,7 +73,7 @@ ggplot(aggregated, aes(x=Scheduler, y=avg_time, fill = Scheduler, group = Schedu
                 width=.2,                    # Width of the error bars
                 position=position_dodge(.9)) +
   theme_fivethirtyeight() + scale_fill_pander() + 
-  theme(axis.title = element_text()) + 
+  theme(axis.title = element_text(), axis.text.x = element_text(angle = 20, hjust = 1)) + 
   ylab('Time [h]') 
 ggsave(filename = "graphs/time_h_scheduler.pdf", device = "pdf", scale = 0.8)
 
@@ -81,8 +83,8 @@ ggplot(aggregated, aes(x=Scheduler, y=avg_energy, fill = Scheduler, group = Sche
                 width=.2,                    # Width of the error bars
                 position=position_dodge(.9)) +
   theme_fivethirtyeight() + scale_fill_pander() + 
-  theme(axis.title = element_text()) + 
-  ylab('Energy [kWh]') 
+  theme(axis.title = element_text(), axis.text.x = element_text(angle = 20, hjust = 1)) + 
+  ylab('Energy [kWh]')
 ggsave(filename = "graphs/energy_kWh_scheduler.pdf", device = "pdf", scale = 0.8)
 
 
@@ -137,6 +139,49 @@ time_energy <- simulations_out %>%
 time_energy$time_ratio <- time_energy$time/time_energy$time_mini
 time_energy$energy_ratio <- time_energy$energy/time_energy$energy_mini
 
+aggregated_time_energy <- time_energy %>%
+  filter(Environment == "combined_cluster") %>%
+  group_by(Scheduler) %>%
+  summarise(avg_time = mean(time_ratio),
+            min_time = min(time_ratio),
+            max_time = max(time_ratio),
+            sd_time = sd(time_ratio),
+            avg_energy = mean(energy_ratio),
+            min_energy = min(energy_ratio),
+            max_energy = max(energy_ratio),
+            sd_energy = sd(energy_ratio))
+
+ggplot(aggregated_time_energy, aes(x=Scheduler, y=avg_time, fill = Scheduler, group = Scheduler)) +
+  geom_bar(stat='identity', position='dodge')  +
+  geom_errorbar(aes(ymin=min_time, ymax=max_time),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) +
+  theme_fivethirtyeight() + scale_fill_pander() + 
+  annotate(geom = "text",label = c("min", "max"),
+           x = c(1,1),
+           y = c(aggregated_time_energy$min_time[1]-0.15,
+                 aggregated_time_energy$max_time[1]+0.15)) + 
+  theme(axis.title = element_text(), axis.text.x = element_text(angle = 20, hjust = 1)) + 
+  ylab('Normalized Time') 
+ggsave(filename = "graphs/time_norm_scheduler.pdf", device = "pdf", scale = 0.8)
+
+ggplot(aggregated_time_energy, aes(x=Scheduler, y=avg_energy, fill = Scheduler, group = Scheduler)) +
+  geom_bar(stat='identity', position='dodge')  +
+  geom_errorbar(aes(ymin=min_energy, ymax=max_energy),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9)) +
+  annotate(geom = "text",label = c("min", "max"),
+           x = c(1,1),
+           y = c(aggregated_time_energy$min_energy[1]-0.1,
+                 aggregated_time_energy$max_time[1]+0.15)) + 
+  theme_fivethirtyeight() + scale_fill_pander() + 
+  theme(axis.title = element_text(), axis.text.x = element_text(angle = 20, hjust = 1)) + 
+  ylab('Normalized Energy')
+ggsave(filename = "graphs/energy_norm_scheduler.pdf", device = "pdf", scale = 0.8)
+
+# Energy and Time combined benchmark
+aggregated_time_energy %>% mutate()
+
 ggplot(time_energy, aes(x=time_ratio, y=energy_ratio, color = Environment)) +
   geom_point()  +
   geom_abline(intercept = 0, slope = 1, linetype = "dotted") +
@@ -149,3 +194,20 @@ ggplot(time_energy, aes(x=time_ratio, y=energy_ratio, color = Environment)) +
   xlab("Time")
 ggsave(filename = "graphs/scheduling_compare.pdf", device = "pdf", scale = 0.8)
 
+
+aggregated <- simulations_out %>%
+  group_by(Scheduler, Environment) %>%
+  summarise(avg_time = mean(time),
+            min_time = min(time),
+            max_time = max(time),
+            avg_energy = mean(energy),
+            min_energy = min(energy),
+            max_energy = max(energy))
+
+ggplot(aggregated, aes(x=Scheduler, y=avg_time, fill = Scheduler, group = Scheduler)) +
+  geom_bar(stat='identity', position='dodge')  +
+  theme_fivethirtyeight() + scale_fill_pander() + 
+  theme(axis.title = element_text(), axis.text.x = element_text(angle = 30, hjust = 1)) + 
+  ylab('Time [s]') + 
+  facet_wrap(~Environment)
+ggsave(filename = "graphs/avg_times_scheduler_env.pdf", device = "pdf", scale = 0.8)
